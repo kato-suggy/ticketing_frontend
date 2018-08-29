ef84410004099926d80168b350d498c83b64bb5b
Kate Sugden <kato.suggy@gmail.com>
2018-08-29 12:54:33 +0100

    mock up of UI elements

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ angular.module("main",[]);
 
  angular.module("main").controller("mainController",function($scope){
     
+    //example of using scope
     $scope.data = "that";
+
+    //for semantic ui dropdown
+    $('.ui.dropdown')
+    .dropdown()
+    ;
+
+    //for semantic ui radio buttons
+    $('.ui.radio.checkbox')
+    .checkbox()
+;
 });
