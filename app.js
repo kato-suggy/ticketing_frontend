eef67f9baf776a62d1838dd041fed5541cbd2086
Kate Sugden <kato.suggy@gmail.com>
2018-08-30 18:01:42 +0100

    replace semantic-ui with bootstrap

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-angular.module("App",["main"]);
+var app = angular.module("App",["main"]);
 
 var main = angular.module("main",[]);
 
 main.controller("mainController", function($scope, $http){
 
+    //this is a promise
     var cinema = $http.get("http://localhost:3000/cinema").
         then(function(response, status, headers, config) {
-            debugger
-            $scope.cinema = response.data;
+            //debugger
+            $scope.cinemas = response.data;
     });
-
-    //for semantic ui dropdown
-    $('.ui.dropdown')
-    .dropdown()
-    ;
-
-    //for semantic ui radio buttons
-    $('.ui.radio.checkbox')
-    .checkbox()
-    ;
 });
