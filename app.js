7826fcb399d9f198a651ed7e2546a9f7be96dccb
Kate Sugden <kato.suggy@gmail.com>
2018-09-05 18:00:07 +0100

    add functionality to choose movie and get screenings

--- app.js
+++ app.js
@@ -4,21 +4,62 @@ var main = angular.module("main",[]);
 
 main.controller("mainController", function($scope, $http){
 
-    //this is a promise
-    cinemas = $http.get("http://localhost:8080/ticketing/cinemas").
-        then(function(response, status, headers, config) {
-            //debugger
-            $scope.cinemas = response.data;
-            //$scope.chosenCinema = $scope.cinemas[0];
-    });
+    $scope.getCinemas = function () {
+        console.log('getting cinemas');
+        $scope.choosingCinema = true;
 
-    //another promise that will hopefully get the movies from a cinema id
-    movies = $http.get("http://localhost:8080/ticketing/movies?cinemaId=2"). //example cinema/2 instead of cinema/{id}
-        then(function(response, status, headers, config) {
-            //debugger
-            $scope.movies = response.data;
-            //$scope.chosenMovie = $scope.movies[0];
-    });
+        cinemas = $http.get("http://localhost:8080/ticketing/cinemas")
+            .then(function(response, status, headers, config) {
+                $scope.cinemas = response.data;
+
+            });
+    }
+
+    $scope.selectCinema = function (cinema) {
+        console.log('meep', cinema)
+        $scope.getMovies(cinema.id);
+        $scope.cinemaSelected = true;
+    }
+
+    $scope.getMovies = function (cinemaId) {
+        console.log('meep', cinemaId);
+        console.log('getting movies');
+
+        movies = $http.get(`http://localhost:8080/ticketing/movies?cinemaId=${cinemaId}`)
+            .then(function(response, status, headers, config) {
+                $scope.movies = response.data;
+                
+            });
+    }
+
+    $scope.selectMovie = function (cinema, movie) {
+        $scope.movieSelected = true;
+        $scope.selectedMovie = movie;
+        console.log('chosen movie');
+
+        $scope.getScreenings(cinema.id, movie.id);
+    }
+
+    $scope.getScreenings = function (cinemaId, movieId) {
+        console.log('meep', cinemaId);
+        console.log('meep', movieId);
+        console.log('getting screenings');
+
+        screenings = $http.get(`http://localhost:8080/ticketing/screenings?cinemaId=${cinemaId}&movieId=${movieId}`)
+            .then(function(response, status, headers, config) {
+                $scope.screenings = response.data;
+            });
+    }
+
+    $scope.selectScreening = function (screeningId) {
+        console.log('meep', screeningId);
+        $scope.screeningSelected = true;
+        $scope.selectedScreening = screening;
+        console.log('chosen screening');
+        //debugger
+        $scope.getSeats(screening);
+    }
+
 
 
 
