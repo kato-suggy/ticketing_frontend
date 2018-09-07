2f6c9c4f8182d6ed1408dcc514e810d768f6fd41
Kate Sugden <kato.suggy@gmail.com>
2018-09-07 15:43:25 +0100

    booking functionality added

--- app.js
+++ app.js
@@ -5,56 +5,48 @@ var main = angular.module("main",[]);
 main.controller("mainController", function($scope, $http){
 
     //variables that need to be submitted to booking function
-    $scope.name = "";
-    $scope.surname = "";
-    $scope.contactNum = "";
-    $scope.email = "";
-    $scope.cardNum = "";
-    $scope.cinemaId = 0;
-    $scope.screenId = 0;
-    $scope.screeningId = 0;
-    $scope.seatNumbers = [];
+    $scope.details = {
+        name: "",
+        surname: "",
+        contactNum: "",
+        email: "",
+        cardNum: "",
+        cinemaId: 0,
+        screenId: 0,
+        screeningId: 0,
+        seatNumbers: []
+    };
 
     $scope.getCinemas = function () {
-        console.log('getting cinemas');
         $scope.booking = true;
-
+        $scope.cancelling = false;
         cinemas = $http.get(`http://localhost:8080/ticketing/cinemas`)
             .then(function(response, status, headers, config) {
                 $scope.cinemas = response.data;
-
             });
     }
 
     $scope.selectCinema = function (cinema) {
-        console.log('meep', cinema)
         $scope.getMovies(cinema.id);
         $scope.cinemaSelected = true;
+        $scope.details.cinemaId = cinema.id;
     }
 
     $scope.getMovies = function (cinemaId) {
-        console.log('meep', cinemaId);
-        console.log('getting movies');
 
         movies = $http.get(`http://localhost:8080/ticketing/movies?cinemaId=${cinemaId}`)
             .then(function(response, status, headers, config) {
-                $scope.movies = response.data;
-                
+                $scope.movies = response.data; 
             });
     }
 
     $scope.selectMovie = function (cinema, movie) {
         $scope.movieSelected = true;
         $scope.selectedMovie = movie;
-        console.log('chosen movie');
-
         $scope.getScreenings(cinema.id, movie.id);
     }
 
     $scope.getScreenings = function (cinemaId, movieId) {
-        console.log('meep', cinemaId);
-        console.log('meep', movieId);
-        console.log('getting screenings');
 
         screenings = $http.get(`http://localhost:8080/ticketing/screenings?cinemaId=${cinemaId}&movieId=${movieId}`)
             .then(function(response, status, headers, config) {
@@ -65,35 +57,48 @@ main.controller("mainController", function($scope, $http){
     $scope.selectScreening = function (screening) {
         $scope.screeningSelected = true;
         $scope.selectedScreening = screening;
-        console.log('chosen screening');
+        $scope.details.screenId = screening.screenId;
+        $scope.details.screeningId = screening.id;
         $scope.getSeats(screening);
     }
 
     $scope.getSeats = function (screening) {
-        console.log('meep', screening.id);
-        console.log('getting seats');
-        console.log('meep', screening.availableSeats);
         $scope.seats = screening.availableSeats;
-
     }
 
     $scope.selectSeat = function (seatNum) {
-        console.log('meep', seatNum);
-        if ($scope.seatNumbers.includes(seatNum)) {
-            $scope.seatNumbers.splice($scope.seatNumbers.indexOf(seatNum), 1);
-            console.log('unselected a seat');
-            console.log('meep', $scope.seatNumbers);
+        if ($scope.details.seatNumbers.includes(seatNum)) {
+            $scope.details.seatNumbers.splice($scope.details.seatNumbers.indexOf(seatNum), 1);
         } else {
-            $scope.seatNumbers.push(seatNum);
-            console.log('selected a seat');
-            console.log('meep', $scope.seatNumbers);
+            $scope.details.seatNumbers.push(seatNum);
         }
-        $scope.seatNumbers.sort();
-        console.log('meep', $scope.seatNumbers);
+        $scope.details.seatNumbers.sort();
     }
 
-    //$scope.getDetails = function () {}
+    $scope.setDetails = function (enterDetails) {
+        $scope.details.name = enterDetails.firstName;
+        $scope.details.surname = enterDetails.lastName;
+        $scope.details.contactNum = enterDetails.mobile;
+        $scope.details.email = enterDetails.email;
+        $scope.details.cardNum = enterDetails.cardNum;
+    }
 
+    $scope.submitDetails = function (enterDetails) {
+        $scope.setDetails(enterDetails);
+        $scope.detailsSubmitted = true;
+        $http.post(`http://localhost:8080/ticketing/createBooking`, $scope.details)
+            .then(function(response, status, headers, config) {
+                $scope.bookingRef = response.data.bookingRef;
+                $scope.detailsSubmitted = true;
+                //$scope.form.$setUntouched();
+                $scope.details={};
+            });
+    }
 
-    
+    $scope.cancelBooking = function (cancelRef) {
+        //check if the ref exists
+        //if so, cancel the booking and confirm
+        //if not, alert booking not found
+    }
+
 });
